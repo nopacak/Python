65f61408be22bea98d322ff3a5202982c6155351
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2024-02-01 15:22:05 +0100

    Create main.py
    Main file for task execution

---
+++ Python_RPA_Projects/insurance_premium_calculation/main.py
@@ -0,0 +1,5 @@
+from tasks import insurance_calculation
+
+# If this script is the one that's directly being used, then do the task defined in robot_crosig_izracun()
+if __name__ == "__main__":
+    insurance_calculation()
